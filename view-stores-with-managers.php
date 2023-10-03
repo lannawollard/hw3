f7811e85ad941e2fd3a8b0342426a242a4e9b430
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-10-03 18:58:11 -0500

    Update view-stores-with-managers.php

--- view-stores-with-managers.php
+++ view-stores-with-managers.php
@@ -7,15 +7,16 @@ while ($store = $stores->fetch_assoc()){
     <div class="card-body">
       <h5 class="card-title"><?php echo $store['store_name']; ?></h5>
       <p class="card-text">
+   <ul class="list-group">
  <?php
   $managers = selectManagesWhatStore($store['store_id']);
   while ($manager = $managers->fetch_assoc()){
   ?>
-    
+     <li class="list-group-item"><?php echo $manager['manager_name']; ?> - <?php echo $manager['floor']; ?> - <?php echo $manager['hours']; ?></li>
   <?php
   }
   ?>
-  
+        </ul>
       </p>
       <p class="card-text"><small class="text-body-secondary">Location: <?php echo $store['store_location']; ?></small></p>
     </div>
