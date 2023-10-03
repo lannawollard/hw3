2de95f5c310a29383efd7a5c6137d2ecb699824a
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-10-03 15:57:44 -0500

    Update managers.php

--- managers.php
+++ managers.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-managers.php);
 
+$pageTitle= "Managers";
+include "view-header.php";
+$stores = selectManagers();
+include "view-stores.php";
+include "view-footer.php";
+?>
