d09ce4d7ad56f5363e0f7d0665b0c14a5ae34d9e
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-10-03 16:31:06 -0500

    Update stores.php

--- stores.php
+++ stores.php
@@ -2,7 +2,7 @@
 require_once("util-db.php");
 require_once("model-stores.php);
 
-$pageTitle= "Stores";
+$pageTitle = "Stores";
 include "view-header.php";
 $stores = selectStores();
 include "view-stores.php";
