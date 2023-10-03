fa62250c9197e9c1cbe1beb71659d21c9b52f5a5
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-10-03 16:01:53 -0500

    Update model-managers.php

--- model-managers.php
+++ model-managers.php
@@ -1,1 +1,15 @@
-
+<?php
+function selectManagers() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT manager_id, manager_name, manager_time FROM `manager`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
