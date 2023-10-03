007429bf8e0f1988aaf449abd1367f563eb8bf1c
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-10-03 17:00:53 -0500

    Create manages-what-store.php

---
+++ manages-what-store.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-manages-what-store.php");
+
+$pageTitle = "Manages what store";
+include "view-header.php";
+$managers = selectManagesWhatStore($_GET['id']);
+include "view-manages-what-store.php";
+include "view-footer.php";
+?>
