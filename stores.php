23b8c3b8de4d9bd7633a612f8e7eb566194a40fc
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-10-03 14:52:56 -0500

    Create stores.php

---
+++ stores.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-stores.php);
+
+$pageTitle= "Stores";
+include "view-header.php";
+$stores = selectStores();
+include "view-stores.php";
+include "view-footer.php";
+?>
