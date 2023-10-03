4af4f6f2395f71b4eaf901926d90a00d77689626
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-10-03 14:33:46 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -20,16 +20,13 @@
           <a class="nav-link active" aria-current="page"href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="post.php">Post</a>
+          <a class="nav-link" href="stores.php">Stores</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="post-result.php">Post result</a>
+          <a class="nav-link" href="managers.php">Managers</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="get.php">Get</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="get-result.php">Get result</a>
+          <a class="nav-link" href="stores-with-managers.php">Stores with managers</a>
         </li>
       </ul>
     </div>
