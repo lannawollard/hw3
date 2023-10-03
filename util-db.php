a01cfbbf12b608c110fc54c7e332c458c22fff17
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-10-03 16:41:56 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,7 +1,7 @@
 <?php
 function get_db_connection(){
     // Create connection
-    $conn = new mysqli('159.89.47.44', 'lannawol_hw3user', 'Youngdolph2021', 'lannawol_hw3');
+    $conn = new mysqli('159.89.47.44', 'lannawol_hw3user', 'Youngdolph2021!', 'lannawol_hw3');
     
     // Check connection
     if ($conn->connect_error) {
