4df53de71fa14c3c93d368dd0db9ab3017a7b1f7
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-10-22 17:04:37 -0500

    Update schedule.php

--- schedule.php
+++ schedule.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-schedule.php");
 
+$pageTitle = "Schedules";
+include "view-header.php";
+$stores = selectSchedule();
+include "view-schedule.php";
+include "view-footer.php";
+?>
