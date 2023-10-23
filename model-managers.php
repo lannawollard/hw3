3f3956e82fd4fd2df271a5c4ca99ea1b30cc811a
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-10-23 12:26:27 -0500

    Update model-managers.php

--- model-managers.php
+++ model-managers.php
@@ -12,4 +12,46 @@ function selectManagers() {
         throw $e;
     }
 }
+function insertManager($mName, $mTime) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `manager` (`manager_name`, `manager_time`) VALUES (?, ?)");
+        $stmt->bind_param("ss", $mName, $mTime);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateManager($mName, $mTime, $mid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `manager` set `manager_name` = ?, `manager_time` = ? where manager_id = ?");
+        $stmt->bind_param("ssi", $mName, $mTime, $mid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteManager($mid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from manager where manager_id=?");
+        $stmt->bind_param("i", $mid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
 ?>
