bcb1b6db81b29ad435164977ef261a18e5385cd0
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-10-23 14:05:49 -0500

    Update managers.php

--- managers.php
+++ managers.php
@@ -4,6 +4,32 @@ require_once("model-managers.php");
 
 $pageTitle = "Managers";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertManager($_POST['mName'], $_POST['mTime'])) {
+        echo '<div class="alert alert-success" role="alert">Manager added.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    case "Edit":
+      if (updateManager($_POST['mName'], $_POST['mTime'], $_POST['mid'])) {
+        echo '<div class="alert alert-success" role="alert">Manager edited.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    case "Delete":
+      if (deleteManager($_POST['mid'])) {
+        echo '<div class="alert alert-success" role="alert">Manager deleted.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+  }
+}
 $managers = selectManagers();
 include "view-managers.php";
 include "view-footer.php";
