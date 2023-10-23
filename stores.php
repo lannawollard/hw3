9d5e03f7b3b19b37696ed4ea4161c51eaa3da32c
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-10-23 10:12:49 -0500

    Update stores.php

--- stores.php
+++ stores.php
@@ -4,6 +4,14 @@ require_once("model-stores.php");
 
 $pageTitle = "Stores";
 include "view-header.php";
+
+if (isset($_POST['actionType'])){
+  switch ($_POST['actionType']) {
+    case "Add":
+    insertStore($_POST['sName'], $_POST['sLocation'], $_POST['sSize']);
+    break;
+  }
+}
 $stores = selectStores();
 include "view-stores.php";
 include "view-footer.php";
