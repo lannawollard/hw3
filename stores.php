bc293bf6fd2b629c46be7f939c0c191e1b9924d1
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-10-23 10:18:16 -0500

    Update stores.php

--- stores.php
+++ stores.php
@@ -8,7 +8,11 @@ include "view-header.php";
 if (isset($_POST['actionType'])){
   switch ($_POST['actionType']) {
     case "Add":
-    insertStore($_POST['sName'], $_POST['sLocation'], $_POST['sSize']);
+    if (insertStore($_POST['sName'], $_POST['sLocation'], $_POST['sSize'])) {
+      echo '<div class="alert alert-success" role="alert">Store added</div>';
+    } else {
+      echo '<div class="alert alert-danger" role="alert">Error</div>';
+    }
     break;
   }
 }
