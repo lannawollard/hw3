be6250849b5a94aecf66b387d2c1cc35180dec60
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-10-23 10:48:42 -0500

    Update manages-what-store.php

--- manages-what-store.php
+++ manages-what-store.php
@@ -4,7 +4,7 @@ require_once("model-manages-what-store.php");
 
 $pageTitle = "Manages what store";
 include "view-header.php";
-$managers = selectManagesWhatStore($_GET['id']);
+$managers = selectManagesWhatStore($_GET['store_id']);
 include "view-manages-what-store.php";
 include "view-footer.php";
 ?>
