f6d10947288d14964f8e548f4eb145db48c93c32
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-10-24 08:44:44 -0500

    Update model-stores-with-managers.php

--- model-stores-with-managers.php
+++ model-stores-with-managers.php
@@ -15,7 +15,7 @@ function selectStores() {
 function selectManagesWhatStore($sid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT m.manager_id, manager_name, manager_time, floor, hours  FROM `manager` m join level l on l.manager_id = m.manager_id WHERE l.store_id=?");
+        $stmt = $conn->prepare("SELECT m.manager_id, manager_name, manager_time, floor, hours, l.level_id  FROM `manager` m join level l on l.manager_id = m.manager_id WHERE l.store_id=?");
         $stmt->bind_param("s", $sid);
         $stmt->execute();
         $result = $stmt->get_result();
