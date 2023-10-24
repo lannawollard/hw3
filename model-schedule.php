aaef6b673ccf1fe658465c28660107ce0954774f
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-10-24 07:28:50 -0500

    Update model-schedule.php

--- model-schedule.php
+++ model-schedule.php
@@ -12,4 +12,46 @@ function selectSchedule() {
         throw $e;
     }
 }
+function insertSchedule($sMorning, $sNight) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `schedule` (`schedule_morning`, `schedule_night`) VALUES (?, ?)");
+        $stmt->bind_param("ss", $sMorning, $sNight);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateSchedule($sMorning, $sNight $sid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `schedule` set `schedule_morning` = ?, `schedule_night` = ? where schedule_id = ?");
+        $stmt->bind_param("ssi", $sMorning, $sNight, $sid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteSchedule($sid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from schedule where schedule_id=?");
+        $stmt->bind_param("i", $sid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
 ?>
