370349a9feb184808ab7e9cfa3465e1deb5045db
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-11-13 15:13:04 -0600

    Update view-stores.php

--- view-stores.php
+++ view-stores.php
@@ -27,3 +27,15 @@ while ($store = $stores->fetch_assoc()){
     </tbody>
   </table>
 </div>
+
+<head>
+	<script src="plotly-2.27.0.min.js" charset="utf-8"></script>
+</head>
+<div id="tester" style="width:600px;height:250px;"></div>
+<script>
+	TESTER = document.getElementById('tester');
+	Plotly.newPlot( TESTER, [{
+	x: [1, 2, 3, 4, 5],
+	y: [1, 2, 4, 8, 16] }], {
+	margin: { t: 0 } } );
+</script>
