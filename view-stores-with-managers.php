b2734f70568d33741e4f5c6d24ee27d12ffc6491
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-11-13 16:39:37 -0600

    Update view-stores-with-managers.php

--- view-stores-with-managers.php
+++ view-stores-with-managers.php
@@ -25,3 +25,84 @@ while ($store = $stores->fetch_assoc()){
 }
 ?>
 </div>
+
+
+
+<script src="https://d3js.org/d3.v6.min.js"></script>
+
+
+<script src="$YOUR_PATH/billboard.js"></script>
+<div id="gaugeChart"></div>
+<script>
+
+
+var chart = bb.generate({
+  data: {
+    columns: [
+	["data", 91.4]
+    ],
+    type: "gauge", // for ESM specify as: gauge()
+    onclick: function(d, i) {
+	console.log("onclick", d, i);
+   },
+    onover: function(d, i) {
+	console.log("onover", d, i);
+   },
+    onout: function(d, i) {
+	console.log("onout", d, i);
+   }
+  },
+  gauge: {},
+  color: {
+    pattern: [
+      "#FF0000",
+      "#F97600",
+      "#F6C600",
+      "#60B044"
+    ],
+    threshold: {
+      values: [
+        30,
+        60,
+        90,
+        100
+      ]
+    }
+  },
+  size: {
+    height: 180
+  },
+  bindto: "#gaugeChart"
+});
+
+setTimeout(function() {
+	chart.load({
+		columns: [["data", 10]]
+	});
+}, 1000);
+
+setTimeout(function() {
+	chart.load({
+		columns: [["data", 50]]
+	});
+}, 2000);
+
+setTimeout(function() {
+	chart.load({
+		columns: [["data", 70]]
+	});
+}, 3000);
+
+setTimeout(function() {
+	chart.load({
+		columns: [["data", 0]]
+	});
+}, 4000);
+
+setTimeout(function() {
+	chart.load({
+		columns: [["data", 100]]
+	});
+}, 5000);
+</script>
+
