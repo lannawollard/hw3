deee61fe22431ed32a5cb572596e55e53ea1aa56
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-11-13 14:21:28 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<div>
+  <canvas id="myChart"></canvas>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+
+
 <?php
 $pageTitle= "Home";
 include "view-header.php";
@@ -6,3 +14,26 @@ include "view-header.php";
 <?php
 include "view-footer.php";
 ?>
+
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+</script>
