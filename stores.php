8cdb904c0998344cd028a90c8d3f50f89c252b7d
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-11-13 15:20:41 -0600

    Update stores.php

--- stores.php
+++ stores.php
@@ -9,14 +9,3 @@ include "view-stores.php";
 include "view-footer.php";
 ?>
 
-<head>
-	<script src="plotly-2.27.0.min.js" charset="utf-8"></script>
-</head>
-<div id="tester" style="width:600px;height:250px;"></div>
-<script>
-	TESTER = document.getElementById('tester');
-	Plotly.newPlot( TESTER, [{
-	x: [1, 2, 3, 4, 5],
-	y: [1, 2, 4, 8, 16] }], {
-	margin: { t: 0 } } );
-</script>
