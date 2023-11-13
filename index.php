d2f8ddfcf5b06ce907d9bf35d345937c0e09370f
lannawollard <142863040+lannawollard@users.noreply.github.com>
2023-11-13 14:25:35 -0600

    Update index.php

--- index.php
+++ index.php
@@ -38,3 +38,86 @@ include "view-footer.php";
     }
   });
 </script>
+
+
+
+        var options = {
+          series: [{
+          name: 'Metric1',
+          data: generateData(18, {
+            min: 0,
+            max: 90
+          })
+        },
+        {
+          name: 'Metric2',
+          data: generateData(18, {
+            min: 0,
+            max: 90
+          })
+        },
+        {
+          name: 'Metric3',
+          data: generateData(18, {
+            min: 0,
+            max: 90
+          })
+        },
+        {
+          name: 'Metric4',
+          data: generateData(18, {
+            min: 0,
+            max: 90
+          })
+        },
+        {
+          name: 'Metric5',
+          data: generateData(18, {
+            min: 0,
+            max: 90
+          })
+        },
+        {
+          name: 'Metric6',
+          data: generateData(18, {
+            min: 0,
+            max: 90
+          })
+        },
+        {
+          name: 'Metric7',
+          data: generateData(18, {
+            min: 0,
+            max: 90
+          })
+        },
+        {
+          name: 'Metric8',
+          data: generateData(18, {
+            min: 0,
+            max: 90
+          })
+        },
+        {
+          name: 'Metric9',
+          data: generateData(18, {
+            min: 0,
+            max: 90
+          })
+        }
+        ],
+          chart: {
+          height: 350,
+          type: 'heatmap',
+        },
+        dataLabels: {
+          enabled: false
+        },
+        colors: ["#008FFB"],
+        title: {
+          text: 'HeatMap Chart (Single color)'
+        },
+        };
+
+        var chart = new ApexCharts(document.querySelector("#chart"), options);
+        chart.render();
